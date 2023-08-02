e8ee1c04c284c6b0e1a355dd4dbc256a9828801f
chojeongeun <juy0816@naver.com>
2023-08-02 12:46:40 +0900

    반복 데이터 출력

--- src/Post.jsx
+++ src/Post.jsx
@@ -3,7 +3,21 @@ import useGetData from './useGetData';
 function Post() {
 	const data = useGetData('https://jsonplaceholder.typicode.com/posts');
 	console.log(data);
-	return <div>Post</div>;
+	return (
+		<div>
+			{data &&
+				data.map((post) => {
+					return (
+						<div key={post.id}>
+							<h2>{post.title}</h2>
+							<br />
+							<p>{post.body}</p>
+							<hr />
+						</div>
+					);
+				})}
+		</div>
+	);
 }
 
 export default Post;
